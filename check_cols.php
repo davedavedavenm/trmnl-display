df970b98a90462641df92e7bb27e0af54b82b746
David <david@davidmagnus.co.uk>
2026-04-21 22:14:53 +0100

    chore: remove legacy InkyPi content and update README

---
+++ check_cols.php
@@ -0,0 +1,6 @@
+﻿<?php
+require '/var/www/html/vendor/autoload.php';
+$app = require '/var/www/html/bootstrap/app.php';
+$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+$cols = Schema::getColumnListing('plugins');
+echo json_encode($cols);
---
+++ get_device.php
@@ -0,0 +1,6 @@
+﻿<?php
+require '/var/www/html/vendor/autoload.php';
+$app = require '/var/www/html/bootstrap/app.php';
+$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+$device = DB::table('devices')->first();
+echo json_encode($device);
